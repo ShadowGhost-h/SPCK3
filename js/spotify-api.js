c7ef0504c6dbfe476d836035c621f001c618ee9f
ShadowGhost-h <minhhoang170106@gmail.com>
2023-09-24 19:16:11 +0700

    up

---
+++ js/spotify-api.js
@@ -0,0 +1,16 @@
+const url = 'https://spotify23.p.rapidapi.com/search/?q=%3CREQUIRED%3E&type=multi&offset=0&limit=10&numberOfTopResults=5';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '6ff04a2c23mshcef18fb576c8237p1dc70djsne8abfc37c6fb',
+		'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
