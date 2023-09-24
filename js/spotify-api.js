cfb0b267bd1b5f2ad25f08e8379f3b4a60559134
ShadowGhost-h <minhhoang170106@gmail.com>
2023-09-24 21:04:48 +0700

    up

--- js/spotify-api.js
+++ js/spotify-api.js
@@ -1,17 +1,20 @@
-const searchtool= document.getElementById('search');
-const url = `https://spotify23.p.rapidapi.com/search/?q=${searchtool.value}&type=multi&offset=0&limit=10&numberOfTopResults=5`;
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '6ff04a2c23mshcef18fb576c8237p1dc70djsne8abfc37c6fb',
-		'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
-	}
-};
+const btn = document.getElementById('btn');
+const input = document.getElementById('search');
+const getdata = async() => {
+  const url = `https://spotify23.p.rapidapi.com/search/?q=${input.value}&type=multi&offset=0&limit=10&numberOfTopResults=5`;  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "6ff04a2c23mshcef18fb576c8237p1dc70djsne8abfc37c6fb",
+      "X-RapidAPI-Host": "spotify23.p.rapidapi.com",
+    },
+  };
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
+  try {
+    const response = await fetch(url, options);
+    const result = await response.text();
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
+};
+btn.addEventListener("click", getdata);
